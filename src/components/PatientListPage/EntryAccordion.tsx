7825da1c6ddaac6de3651f5a5a8da0ac44d12d2b
Atte <atte.lappalainen@hotmail.com>
2024-06-06 21:31:14 +0300

    add diagnosis full info

--- src/components/PatientListPage/EntryAccordion.tsx
+++ src/components/PatientListPage/EntryAccordion.tsx
@@ -1,8 +1,10 @@
-import { Entry } from "../../types"
+import { Diagnosis, Entry } from "../../types"
 import Accordion from '@mui/material/Accordion';
 import AccordionSummary from '@mui/material/AccordionSummary';
 import AccordionDetails from '@mui/material/AccordionDetails';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import { useEffect, useState } from "react";
+import { getByCode } from "../../services/diagnoses";
 
 
 interface Props{
@@ -10,8 +12,20 @@ interface Props{
 }
 
 export const EntryAccordion = ({entry}: Props) => {
+    const [diagnoses, setDiagnoses] = useState<Diagnosis[]>([])
+    useEffect(() => {
+        if (diagnoses.length === 0) {
+            
+        
+        entry.diagnosisCodes?.forEach(d => {
+            getByCode(d)
+            .then(result => setDiagnoses([...diagnoses, result]))
+            .catch(e => console.log(e))
+        })
+        }
+    },[])
 
-    
+    console.log(diagnoses)
     return (
         <div>
             <Accordion>
@@ -27,8 +41,10 @@ export const EntryAccordion = ({entry}: Props) => {
             {entry.description}
             <div>
                 <h4>Diagnoses:</h4>
-                {entry.diagnosisCodes && entry.diagnosisCodes.map((d) => (
-                    <p key={d}>{d}</p>
+                {diagnoses && diagnoses.map((d) => (
+                    <div key={d.code}>
+                        {d.code} {d.name}     <i> {d.latin}</i>
+                    </div>
                 ))}
             </div>
             </AccordionDetails>
